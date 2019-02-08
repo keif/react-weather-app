e07d70ddc0ed777403f4ccb53f1e98212eaddced
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-02-08 01:05:51 -0500

    Add in weather component and API_KEY reference.

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,18 @@ import "./App.css";
 
 import Titles from "./../components/Titles";
 import Form from "./../components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = process.env.REACT_APP_WEATHER_API;
 
 class App extends Component {
 	render() {
 		return (
-      <div>
-        <Titles /> 
-        <Form />
-      </div>
+			<div>
+				<Titles />
+				<Form />
+				<Weather />
+			</div>
 		);
 	}
 }
